a9e988be5ecdc68e754584410df8c8f03054f313
arsyad1706 <49345220+arsyad1706@users.noreply.github.com>
2023-02-08 12:14:31 +0700

    Update ItemProdukLayananController.php

--- app/Http/Controllers/Backend/ItemProdukLayananController.php
+++ app/Http/Controllers/Backend/ItemProdukLayananController.php
@@ -207,7 +207,7 @@ class ItemProdukLayananController extends Controller
     public function update(Request $request, $id)
     {
         // return $request->all();
-        // return $request->file('cover')->getClientOriginalName();
+        return $request->file('cover')->getClientOriginalName();
         $konten = ItemProdukLayanan::find($id);
 
         $isUnique = $konten->judul == $request->judul ? '' : '|unique:item_produk_layanan,judul';
